2e0df6de1561012517d60cde73b34b563899f665
jbtwitt <jbtwitt@outlook.com>
2017-06-13 18:28:28 -0400

    mSnapshot.py

--- motion-detect-recorder/mSnapshot.py
+++ motion-detect-recorder/mSnapshot.py
@@ -2,17 +2,32 @@ from time import sleep
 import datetime
 import json
 
-def main(mdrConf):
+def snapshot(mdrConf):
     from picamera import PiCamera
     res = mdrConf['camera']['resolution']
     camera = PiCamera()
     camera.resolution = (res[0], res[1])
-    # camera.framerate = 15
     camera.start_preview()
-    sleep(5)
-    camera.capture('/home/pi/Desktop/max.jpg')
+    # sleep(5)
+    jpgName = filename(mdrConf)
+    camera.capture(jpgName)
     camera.stop_preview()
+    return jpgName
+
+
+def filename(mdrConf):
+    timestamp = datetime.datetime.now()
+    return mdrConf['camCacheRepository'] + '/snapshot_' + timestamp.strftime("%Y%m%d_%H%M%S_%f") + '.jpg'
+
+
+def main(mdrConf):
+    jpgFiles = []
+    jpgFiles.append(snapshot(mdrConf))
+    mdrMail = MdrMail(mdrConf)
+    mdrMail.attachImages(jpgFiles)
+    mdrMail.send()
+
 
 if __name__ == "__main__":
     mdrConf = json.load(open('mdr.json'))
-    print datetime.datetime.now()
+    main()
