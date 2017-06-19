526dc896d5e3b8ccdb6ec29cb738de0eed9c148b
jbtwitt <jbtwitt@outlook.com>
2017-06-19 19:07:18 -0400

    test

--- test_cv2camera.py
+++ test_cv2camera.py
@@ -1,20 +1,83 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def arrs2Contours(arrs):
+    contours = []
+    for arr in arrs:
+        contours.append(np.array(arr))
+    return contours
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def testContours():
+    import glob
+    import json
+    dir = '/tmp/snapshot/'
+    jsonFiles = glob.glob(dir + '*.json')
+    mdRecords = json.load(open(jpgFiles[0]))
+    idxs = mdRecords.keys()
+    for idx in idxs:
+        cntStr = mdRecords[idx][1]
+        cnts = arrs2Contours(cntStr)
+        moments = cv2.moments(cnts)
+        cX = int(moments["m10"] / moments["m00"])
+        cY = moments(M["m01"] / moments["m00"])
+        print "center: " + str(cX) + ', ' + str(cY)
+        print moments['m00']    # area
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        jpgFile = mdRecords[idx][0]
+        im = cv2.imread(jpgFile)
+        cv2.drawContours(im, cnts, -1, (0,155,0), 1)
+        cv2.imshow('frame', im)
+        while(True):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cv2.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+def testSnapshot():
+    import os
+    dir = '/tmp/snapshot/'
+    jpgFiles = os.listdir(dir)
+    for i in range(2):
+        cnts = diff2JpgFiles(dir + jpgFiles[i], dir + jpgFiles[i + 1])
+        if len(cnts) > 0:
+            im = cv2.imread(dir + jpgFiles[i + 1])
+            cv2.drawContours(im, cnts, -1, (0,155,0), 1)
+            # centers = findContoursCenters(cnts)
+            # for c in centers:
+            #     cv2.circle(im, (c[0], c[1]), 3, (255, 255, 255), -1)
+            moments = cv2.moments(cnts)
+            cv2.imwrite('/tmp/test_' + str(i) + '.jpg', im)
+            print 'contours: ' + str(len(cnts))
+            # test contours to string
+            jsonStr = '{"cnts":' + contours2ArrsStr(cnts) + '}'
+            jsonObj = json.loads(jsonStr)
+            im = cv2.imread(dir + jpgFiles[i + 1])
+            cv2.drawContours(im, arrs2Contours(jsonObj['cnts']), -1, (0,155,0), 1)
+            cv2.imwrite('/tmp/test_' + str(i) + str(i) + '.jpg', im)
+
+
+def testCV2Camera():
+    cap = cv2.VideoCapture(0)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        # cv2.imshow('frame',gray)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    testContours()
+    # import motion-detect-recorder.classes.MdrUtil
+    # mdrConfJsonFile = 'mdr.json'
+    # mdrConf = json.load(open(mdrConfJsonFile))
