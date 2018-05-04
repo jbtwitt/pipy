c4c2700febbd37430d5f88646da8e27e6d4eeefd
jbtwitt <jbtwitt@outlook.com>
2018-05-03 20:17:35 -0400

    hqmetafile

--- hq/HqMetaFile.py
+++ hq/HqMetaFile.py
@@ -1,21 +1,28 @@
 import json
 import os
+import pandas as pd
 from datetime import datetime
 
-JsonFile = "{}/{}.meta.json"
+JsonFile = "{}/hqMeta{}.json"
 class HqMetaFile:
 
-    def __init__(self, hqConf):
-        repo = hqConf["repo"]
-        self.metaFile = JsonFile.format(repo, datetime.now().strftime("%Y%m%d"))
-        # print(self.metaFile)
+    def __init__(self, hqConf, day):
+        self.hqConf = hqConf
+        self.day = day
 
-    def write(self, hqMeta):
-        with open(self.metaFile, 'w') as outfile:
+    def writeJson(self, hqMeta):
+        hqMetaFile = JsonFile.format(self.hqConf['repo'], self.day)
+        # print(hqMetaFile)
+        with open(hqMetaFile, 'w') as outfile:
             json.dump(hqMeta, outfile)
 
-    def read(self):
-        hqMetas = json.load(open(self.metaFile))
+    def readJson(self):
+        hqMetaFile = JsonFile.format(self.hqConf['repo'], self.day)
+        hqMetas = json.load(open(hqMetaFile))
         for hqMeta in hqMetas:
             print(hqMeta)
         return hqMetas
+
+    def toCsv(self):
+        hqMetaFile = JsonFile.format(self.hqConf['repo'], self.day)
+        return pd.read_json(hqMetaFile)
