11aabdf74a56a9afe0957e88975dd5dc4f1a9119
jb <jbtwitt@outlook.com>
2019-10-28 21:08:58 -0400

    add pi-monitor-main

---
+++ pi-monitor-main.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+from time import sleep
+from picamera import PiCamera
+
+REPOS = "/tmp/pi-monitor"
+RECYCLE_DAYS = 3
+CAMERA_RESOLUTION = [800, 608]
+
+def createFolder():
+    for i in range(RECYCLE_DAYS):
+        dir = "%s/%d" % (REPOS, i)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+def getPath(extName='jpg'):
+    tm = datetime.now()
+    return "%s/%d/%s.%s" % (REPOS, (tm.timetuple().tm_yday % RECYCLE_DAYS), tm.strftime("%H%M%S"), extName)
+
+def cameraShot():
+    camera = PiCamera()
+    try:
+        camera.resolution = CAMERA_RESOLUTION
+        # warming
+        camera.start_preview()
+        jpgName = getPath()
+        camera.capture(jpgName)
+        camera.stop_preview()
+
+        while True:
+            sleep(2)
+            jpgName = getPath()
+            camera.capture(jpgName)
+    finally:
+        camera.close()
+
+def main():
+    createFolder()
+    cameraShot()
+
+if __name__ == "__main__":
+    main()
