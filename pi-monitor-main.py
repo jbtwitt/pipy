42494a8e792caba5c422fb57d3379c01f64af0ab
jb <jbtwitt@outlook.com>
2019-10-31 20:45:51 -0400

    add width height args

--- pi-monitor-main.py
+++ pi-monitor-main.py
@@ -4,7 +4,7 @@ from time import sleep
 
 REPOS = "/tmp/pi-monitor"
 RECYCLE_DAYS = 3
-CAMERA_RESOLUTION = [1280, 960]
+CAMERA_RESOLUTION = [640, 480]
 
 def createFolder():
     for i in range(RECYCLE_DAYS):
@@ -25,7 +25,7 @@ def camera_main():
         camera = PiCamera()
         try:
             camera.resolution = CAMERA_RESOLUTION
-            # warming
+            # camera warming
             camera.start_preview()
             jpgName = getPath(datetime.now())
             camera.capture(jpgName)
@@ -59,23 +59,23 @@ def web_main():
             if minutesAgo > 0:
                 tm = tm - timedelta(minutes=minutesAgo)
             jpgFile = getPath(tm)
-            # print(jpgFile)
-            if os.path.exists(jpgFile) and os.stat(jpgFile).st_size > 1000:
+            jpgStat = os.stat(jpgFile)
+            if os.path.exists(jpgFile) and jpgStat.st_size > 1000 and (datetime.now() - datetime.fromtimestamp(jpgStat.st_ctime)).total_seconds() < 5:
                 response = Response()
-                # response.headers.add('Connection', 'close')
-                response.headers.add('Content-Lenght', str(os.path.getsize(jpgFile)))
+                response.headers.add('Content-Lenght', str(jpgStat.st_size))
                 return send_file(jpgFile, mimetype='image/jpg', cache_timeout=0, as_attachment=False, add_etags=False)
             sleep(0.5)
         return ""
 
     app.run(host='0.0.0.0', threaded=True)
 
-import argparse
+import sys
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('mainName', metavar='web_main | camera_main', type=str)
-    args = parser.parse_args()
-    if args.mainName == 'web_main':
+    if sys.argv[1] == 'web_main':
         web_main()
-    else:
+    elif sys.argv[1] == 'camera_main':
+        if len(sys.argv) == 4:
+            CAMERA_RESOLUTION = [int(sys.argv[2]), int(sys.argv[3])]
         camera_main()
+    else:
+        print('Usage: python %s web_main|camera_main [width height]' % (sys.argv[0]))
